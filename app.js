608c9286165d09190d181e94b754f51645000d74
roocheerah <ruchirakulkarni@D-173-250-180-138.dhcp4.washington.edu>
2016-05-24 12:14:35 -0700

    changing spot stuff

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var particle = new Particle();
 var db = new firebase("https://capstoneee475.firebaseio.com/");
 var prev_values = [];
 
+var flag = true;
 
 
 server.listen(port, function () {
@@ -26,6 +27,12 @@ app.use(express.static(__dirname + '/public'));
 io.sockets.on("connection", function (socket) {  
     // to make things interesting, have it send every second
     var interval = setInterval(function () {
+      if (flag) {
+        flag = false;
+        getNumVariable('310047000447343232363230');
+        //console.log(num_vars);
+      }
+
       //myFirebaseRef.once("value", function(snapshot) {
       db.on("value", function(snapshot) {  
         snapshot.forEach(function(childSnapshot) {
@@ -38,6 +45,45 @@ io.sockets.on("connection", function (socket) {
       });
     }, 50);
 
+    function getNumVariable(id) {
+      particle.getVariable({ deviceId: id,
+                               name: "num_vars",
+                               auth: 'f8093528e7b81caceeaecd0569423df524dffbab'
+                             }).then(function(data) {
+                                console.log('Device variable retrieved successfully:', data.body);
+                                result = data.body.result;
+                                console.log("result: " + result);
+                                for(var i = 0; i < result; i++) {
+                                  var spot = "spot_" + i;
+                                  console.log("Inside the for-loop with spot: " + spot);
+                                  getDataFromParticleCloud(spot, id); //310047000447343232363230
+                                }
+                                //return result;
+                              }, function(err) {
+                                //console.log('An error occurred while getting attrs:', err);
+                              });                          
+    }
+
+
+    // Need a global flag for sampling each spot according to the variable
+    function getDataFromParticleCloud(variable, id) {
+        particle.getVariable({ deviceId: id,
+                               name: variable,
+                               auth: 'f8093528e7b81caceeaecd0569423df524dffbab'
+                             }).then(function(data) {
+                                console.log('Device variable retrieved successfully:', data.body.result);
+                                //spots[variable] = data.body.occupied;
+                                db.child(variable).set(data.body.result);
+
+                                      // If occupied, update the table, 
+                                // If not occupied, "Vacant"
+                              }, function(err) {
+                                //console.log('An error occurred while getting attrs:', err);
+                              });                        
+    }
+
+
+    // Everytime an event changes call this function
     particle.getEventStream({name: 'EE475Capstone-SpotChanged', auth: 'f8093528e7b81caceeaecd0569423df524dffbab'}).then(function(stream) {
       stream.on('event', function(data) {
         // Gives the spot number
